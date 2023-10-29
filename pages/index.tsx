87fcd56abf162101fbb51966b9b4fa6628395721
Stephon <shonuff4521@gmail.com>
2023-10-28 20:23:38 -0400

    1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
-import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { Navbar } from "@/components/NavBar";
+
 
 export default function HomePage() {
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
+      <Navbar />
     </>
   );
 }
